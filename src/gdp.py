7dbae33a0e556af5da85dc8d5e56f35d6c029857
氡钫镭锕钍 <287685124@qq.com>
2021-06-23 19:23:15 +0800

    1

--- src/gdp.py
+++ src/gdp.py
@@ -14,17 +14,38 @@ def load_data(path):
     return data
 
 data = load_data("data/gdp.csv")
-st.write(data)
+if st.checkbox('show raw data'):
+    st.write(data)
+
+if st.checkbox('Show all gdp'):
+    st.subheader('all')
+    all_data = pd.DataFrame(data.values.T, index=data.columns, columns=data["country name"].unique())[4:]
+    st.line_chart(all_data)
 
 product_list = data["country name"].unique()
-product_type = st.sidebar.selectbox(
-    "Which kind of event do you want to explore?",
-    product_list
-)
-part_data =[(data["country name"] == product_type)]
+print(product_list)
 
-st.title(f"{product_type}的GDP折线图")
-sub_data = data[(data["country name"] == product_type)]
-sub_data2 = pd.DataFrame(sub_data.values.T, index=sub_data.columns, columns=sub_data.index)[4:]
-print(sub_data2)
-st.line_chart(sub_data2)
+product_type = st.sidebar.selectbox(
+    "Which kind of event do you want to compare?",
+    product_list,
+    key = 'ada'
+)
+
+product_type_2 = st.sidebar.selectbox(
+    "Which kind of event do you want to compare?",
+    product_list,
+    key = 'ava'
+)
+
+if(product_type != product_type_2):
+    st.title(f"{product_type} vs {product_type_2} 的GDP对比图")
+    sub_data = data[(data["country name"] == product_type) | (data["country name"] == product_type_2)]
+    sub_data2 = pd.DataFrame(sub_data.values.T, index=sub_data.columns, columns=[product_type, product_type_2])[4:]
+
+    st.line_chart(sub_data2)
+
+else:
+    st.title(f"{product_type}的GDP折线图")
+    sub_data = data[(data["country name"] == product_type) | (data["country name"] == product_type_2)]
+    sub_data2 = pd.DataFrame(sub_data.values.T, index=sub_data.columns, columns=[product_type])[4:]
+    st.line_chart(sub_data2)
